3c9b67b081312ef93505c7d31081d24fd7bb3272
AniketDhumal <150125078+AniketDhumal@users.noreply.github.com>
2025-10-05 12:02:08 +0530

    Update register.js

--- frontend/js/register.js
+++ frontend/js/register.js
@@ -90,7 +90,7 @@ document.addEventListener('DOMContentLoaded', () => {
       }
 
       try {
-        const res = await fetch('http://localhost:5000/api/v1/auth/register', {
+        const res = await fetch('https://tuition-c67n.onrender.com/api/v1/auth/register', {
           method: 'POST',
           headers: { 'Content-Type': 'application/json' },
           body: JSON.stringify({
